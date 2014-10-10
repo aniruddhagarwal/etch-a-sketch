3ca1f4de92269e1ec1162f2979707e00b7376a1e
Aniruddh Agarwal <aaniruddh99@gmail.com>
2014-10-11 07:51:29 +0800

    finished the project

--- application.js
+++ application.js
@@ -1,13 +1,26 @@
-$(document).ready(function() {
-	for(var i = 1; i <= 16; i++) {
+function create_grid(sof) {
+	$("#wrapper").html("");
+	for(var i = 0; i < sof; i++) {
 		$("#wrapper").append("<div class = 'row'></div>");
 	}
 
-	for(var p = 1; p <= 16; p++) {
-		$(".row").append("<div class = 'box'></div>");		
+	for(var p = 1; p <= sof; p++) {
+		$(".row").append("<div class = 'cell'></div>");		
 	}
 
-	$(".box").on("mouseenter", function() {
-		$(this).addClass("visited");
+	var size = 640 / sof;
+	
+	$(".cell").width(size);
+	$(".cell").height(size);
+}
+
+$(document).ready(function() {
+	$(".btn").on("click", function() {
+		var size_of_grid = prompt("Please enter the number of cells you want per row.", "16");
+		create_grid(size_of_grid);
+
+		$(".cell").hover(function() {
+			$(this).css("background-color", "white");
+		});
 	});
 });
